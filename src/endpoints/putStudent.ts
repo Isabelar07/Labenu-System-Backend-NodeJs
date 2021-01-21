c254d4a74ef2eccbc1e1f3fae20274b1774b92f3
Vinicius Silveira Moraes <vinicius_jag@hotmail.com>
2021-01-21 10:24:08 -0300

    split consertado

--- src/endpoints/putStudent.ts
+++ src/endpoints/putStudent.ts
@@ -7,26 +7,22 @@ export const putStudent = async(req: Request, res: Response): Promise<void> => {
 
     try {
 
-        let [day, month, year] = req.body.birthDateSql.split("/")
-        day++
-        const birthDate: Date = new Date(`${year}-${month}-${day}`)
-
-        const { id, name, email, mission_id } = req.body as student 
-
-
-        if (!id || !name || !email || !birthDate || !mission_id) {
+        const [dia,mes,ano] = req.body.birthdate.split("/")        
+        const newDate: Date = new Date(`${ano}-${mes}-${dia}`)
+        
+        const {id,name,email,mission_id} =  req.body as student
+        if(!id || !name || !email || !newDate || !mission_id){
             throw new Error("Todos os campos são obrigatórios")
         }
-
+        
         const newStudent: any = await insertStudent(
             id,
             name,
             email,
-            birthDate,
+            newDate,
             mission_id
         )
-
-        res.status(200).send({message: "Estudante criado com sucesso!", newStudent})
+        res.status(200).send({message:"Estudante criado com sucesso!",newStudent})
 
     } catch(error) {
         console.log(error)
